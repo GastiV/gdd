d4603f3ae61ed970290ce2f15e6e083ab6b6eebc
Gasti Vidal <gvidal@est.frba.utn.edu.ar>
2020-07-14 08:10:15 -0300

    Merge y fix nombre de grupo

--- ScriptsDB/script_creacion_BI.sql
+++ ScriptsDB/script_creacion_BI.sql
@@ -1,13 +1,13 @@
 PRINT 'Comienzo creación de tabla del modelo BI';
 
-CREATE TABLE LA_EMPRESA.bi_anio_y_mes(
+CREATE TABLE LOW_STRESS_DESIGN.bi_anio_y_mes(
     id INTEGER IDENTITY(0,1) PRIMARY KEY,
     anio INT,
     mes INT CHECK (mes BETWEEN 1 AND 12)
 );
 PRINT 'Creada tabla Año y Mes';
 
-CREATE TABLE LA_EMPRESA.bi_cliente (
+CREATE TABLE LOW_STRESS_DESIGN.bi_cliente (
     id INTEGER PRIMARY KEY,
     dni INTEGER NOT NULL,
     apellido VARCHAR(50) NOT NULL,
@@ -18,42 +18,42 @@ CREATE TABLE LA_EMPRESA.bi_cliente (
 );
 PRINT 'Creada tabla Cliente BI';
 
-CREATE TABLE LA_EMPRESA.bi_proveedor (
+CREATE TABLE LOW_STRESS_DESIGN.bi_proveedor (
     id INTEGER PRIMARY KEY,
     razon_social VARCHAR(100) NOT NULL
 );
 PRINT 'Creada tabla Proveedor BI';
 
-CREATE TABLE LA_EMPRESA.bi_ciudad (
+CREATE TABLE LOW_STRESS_DESIGN.bi_ciudad (
     codigo INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
 );
 PRINT 'Creada tabla ciudad BI.';
 
-CREATE TABLE LA_EMPRESA.bi_tipo_habitacion (
+CREATE TABLE LOW_STRESS_DESIGN.bi_tipo_habitacion (
     codigo INT PRIMARY KEY,
     descripcion VARCHAR(50) NOT NULL
 );
 PRINT 'Creada tabla bi_tipo_habitacion.';
 
-CREATE TABLE LA_EMPRESA.bi_avion (
+CREATE TABLE LOW_STRESS_DESIGN.bi_avion (
     identificador VARCHAR(50) NOT NULL PRIMARY KEY,
     modelo VARCHAR(50)
 );
 PRINT 'Creada tabla bi_avion.';
 
-CREATE TABLE LA_EMPRESA.bi_ruta_aerea (
+CREATE TABLE LOW_STRESS_DESIGN.bi_ruta_aerea (
     codigo INT PRIMARY KEY
 );
 PRINT 'Creada tabla bi_ruta_aerea.'
 
-CREATE TABLE LA_EMPRESA.bi_tipo_pasaje (
+CREATE TABLE LOW_STRESS_DESIGN.bi_tipo_pasaje (
     id INT IDENTITY(0,1) PRIMARY KEY,
     descripcion VARCHAR(50)
 );
 PRINT 'Creada bi_tipo_pasaje.'
 
-CREATE TABLE LA_EMPRESA.fact_table_estadia (
+CREATE TABLE LOW_STRESS_DESIGN.fact_table_estadia (
     id_anio_y_mes INT NOT NULL,
     id_cliente INT NOT NULL,
     id_empresa INT NOT NULL,
@@ -63,22 +63,22 @@ CREATE TABLE LA_EMPRESA.fact_table_estadia (
     cantidad_de_camas_vendidas INT NOT NULL,
     cantidad_de_habitaciones_vendidas INT NOT NULL,
     ganancias_realizadas INT NOT NULL,
-    FOREIGN KEY (id_anio_y_mes) REFERENCES LA_EMPRESA.bi_anio_y_mes (id),
-    FOREIGN KEY (id_cliente) REFERENCES LA_EMPRESA.bi_cliente (id),
-    FOREIGN KEY (id_empresa) REFERENCES LA_EMPRESA.bi_proveedor (id),
-    FOREIGN KEY (id_tipo_habitacion) REFERENCES LA_EMPRESA.bi_tipo_habitacion (codigo),
+    FOREIGN KEY (id_anio_y_mes) REFERENCES LOW_STRESS_DESIGN.bi_anio_y_mes (id),
+    FOREIGN KEY (id_cliente) REFERENCES LOW_STRESS_DESIGN.bi_cliente (id),
+    FOREIGN KEY (id_empresa) REFERENCES LOW_STRESS_DESIGN.bi_proveedor (id),
+    FOREIGN KEY (id_tipo_habitacion) REFERENCES LOW_STRESS_DESIGN.bi_tipo_habitacion (codigo),
     PRIMARY KEY (id_anio_y_mes, id_cliente, id_empresa, id_tipo_habitacion)
 );
 PRINT 'Creada Fact Table de Estadias';
 
-CREATE TABLE LA_EMPRESA.fact_table_pasaje (
-    id_anio_y_mes INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.bi_anio_y_mes (id),
-    id_cliente INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.bi_cliente (id),
-    id_empresa INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.bi_proveedor (id),
-    id_ciudad_orig INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.bi_ciudad (codigo),
-    id_ciudad_dest INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.bi_ciudad (codigo),
-    id_avion VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.bi_avion (identificador),
-    codigo_ruta_aerea INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.bi_ruta_aerea (codigo),
+CREATE TABLE LOW_STRESS_DESIGN.fact_table_pasaje (
+    id_anio_y_mes INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.bi_anio_y_mes (id),
+    id_cliente INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.bi_cliente (id),
+    id_empresa INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.bi_proveedor (id),
+    id_ciudad_orig INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.bi_ciudad (codigo),
+    id_ciudad_dest INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.bi_ciudad (codigo),
+    id_avion VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.bi_avion (identificador),
+    codigo_ruta_aerea INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.bi_ruta_aerea (codigo),
     precio_promedio_compra INT NOT NULL,
     precio_promedio_venta INT NOT NULL,
     cantidad_de_pasajes_aereos_vendidos INT NOT NULL,
@@ -91,51 +91,51 @@ PRINT 'Comienzo carga del modelo BI';
 
 PRINT 'El mismo se compone de dos Fact tables con sus respectivas dimensiones';
 
-INSERT INTO LA_EMPRESA.bi_ciudad (codigo, nombre)
-SELECT codigo, nombre FROM LA_EMPRESA.ciudad;
+INSERT INTO LOW_STRESS_DESIGN.bi_ciudad (codigo, nombre)
+SELECT codigo, nombre FROM LOW_STRESS_DESIGN.ciudad;
 
 PRINT 'Migradas ciudades al modelo BI';
 
-INSERT INTO LA_EMPRESA.bi_cliente (id, dni, apellido, nombre, email, fecha_nac, telefono)
-SELECT id, dni, apellido, nombre, email, fecha_nac, telefono FROM LA_EMPRESA.cliente;
+INSERT INTO LOW_STRESS_DESIGN.bi_cliente (id, dni, apellido, nombre, email, fecha_nac, telefono)
+SELECT id, dni, apellido, nombre, email, fecha_nac, telefono FROM LOW_STRESS_DESIGN.cliente;
 
 PRINT 'Migrados clientes al modelo BI';
 
-INSERT INTO LA_EMPRESA.bi_proveedor (id, razon_social)
-SELECT id, razon_social FROM LA_EMPRESA.empresa;
+INSERT INTO LOW_STRESS_DESIGN.bi_proveedor (id, razon_social)
+SELECT id, razon_social FROM LOW_STRESS_DESIGN.empresa;
 
 PRINT 'Migrados proveedores al modelo BI';
 
-INSERT INTO LA_EMPRESA.bi_anio_y_mes (anio, mes)
-SELECT DISTINCT YEAR(f.fecha), MONTH(f.fecha) FROM LA_EMPRESA.factura f;
+INSERT INTO LOW_STRESS_DESIGN.bi_anio_y_mes (anio, mes)
+SELECT DISTINCT YEAR(f.fecha), MONTH(f.fecha) FROM LOW_STRESS_DESIGN.factura f;
 
 PRINT 'Migradas fechas con actividad al modelo BI';
 
-INSERT INTO LA_EMPRESA.bi_tipo_habitacion (codigo, descripcion)
+INSERT INTO LOW_STRESS_DESIGN.bi_tipo_habitacion (codigo, descripcion)
 SELECT codigo, descripcion
-FROM LA_EMPRESA.tipo_habitacion
+FROM LOW_STRESS_DESIGN.tipo_habitacion
 
 PRINT 'Migrados tipos de habitaciones al modelo BI';
 
-INSERT INTO LA_EMPRESA.bi_avion (identificador, modelo)
+INSERT INTO LOW_STRESS_DESIGN.bi_avion (identificador, modelo)
 SELECT identificador, modelo
-FROM LA_EMPRESA.avion
+FROM LOW_STRESS_DESIGN.avion
 
 PRINT 'Migrados aviones al modelo BI';
 
-INSERT INTO LA_EMPRESA.bi_ruta_aerea (codigo)
+INSERT INTO LOW_STRESS_DESIGN.bi_ruta_aerea (codigo)
 SELECT DISTINCT codigo
-FROM LA_EMPRESA.ruta_aerea
+FROM LOW_STRESS_DESIGN.ruta_aerea
 
 PRINT 'Migradas rutas aereas al modelo BI';
 
-INSERT INTO LA_EMPRESA.bi_tipo_pasaje (descripcion)
+INSERT INTO LOW_STRESS_DESIGN.bi_tipo_pasaje (descripcion)
 SELECT DISTINCT tipo
-FROM LA_EMPRESA.butaca
+FROM LOW_STRESS_DESIGN.butaca
 
 PRINT 'Migradas tipos de pasaje al modelo BI';
 
-INSERT INTO LA_EMPRESA.fact_table_estadia (id_anio_y_mes, id_cliente, id_empresa, id_tipo_habitacion,cantidad_de_camas_vendidas, precio_promedio_compra, precio_promedio_venta, cantidad_de_habitaciones_vendidas, ganancias_realizadas)
+INSERT INTO LOW_STRESS_DESIGN.fact_table_estadia (id_anio_y_mes, id_cliente, id_empresa, id_tipo_habitacion,cantidad_de_camas_vendidas, precio_promedio_compra, precio_promedio_venta, cantidad_de_habitaciones_vendidas, ganancias_realizadas)
 SELECT
     aym.id,
     f.id_cliente,
@@ -154,17 +154,17 @@ SELECT
     AVG(h.precio) 'Promedio de Venta',
     COUNT(*) 'Habitaciones Vendidas',
     (SUM(h.precio) - SUM(h.costo)) 'Ganancias Realizadas'
-FROM LA_EMPRESA.factura f
-JOIN LA_EMPRESA.estadia e ON (e.codigo = f.id_servicio)
-JOIN LA_EMPRESA.habitacion h ON (h.id_habitacion = e.id_habitacion)
-JOIN LA_EMPRESA.bi_anio_y_mes aym ON (aym.anio = YEAR(f.fecha) AND aym.mes = MONTH(f.fecha))
-JOIN LA_EMPRESA.servicio s ON (s.codigo = f.id_servicio)
-JOIN LA_EMPRESA.compra co ON (co.numero = s.id_compra)
+FROM LOW_STRESS_DESIGN.factura f
+JOIN LOW_STRESS_DESIGN.estadia e ON (e.codigo = f.id_servicio)
+JOIN LOW_STRESS_DESIGN.habitacion h ON (h.id_habitacion = e.id_habitacion)
+JOIN LOW_STRESS_DESIGN.bi_anio_y_mes aym ON (aym.anio = YEAR(f.fecha) AND aym.mes = MONTH(f.fecha))
+JOIN LOW_STRESS_DESIGN.servicio s ON (s.codigo = f.id_servicio)
+JOIN LOW_STRESS_DESIGN.compra co ON (co.numero = s.id_compra)
 GROUP BY aym.id, f.id_cliente, co.id_empresa, h.id_tipo_habitacion;
 
 PRINT 'Llenado de Fact Table Estadia completo';
 
-INSERT INTO LA_EMPRESA.fact_table_pasaje (
+INSERT INTO LOW_STRESS_DESIGN.fact_table_pasaje (
 	id_anio_y_mes,
 	id_cliente,
 	id_empresa,
@@ -188,13 +188,13 @@ SELECT
 	AVG(p.precio) 'precio_promedio_venta',
 	COUNT(*) 'cantidad_de_pasajes_aereos_vendidos',
 	SUM(p.precio - p.costo) 'ganancias_realizadas'
-FROM LA_EMPRESA.pasaje p
-JOIN LA_EMPRESA.factura f ON f.id_servicio = p.codigo
-JOIN LA_EMPRESA.bi_anio_y_mes aym ON (aym.anio = YEAR(f.fecha) AND aym.mes = MONTH(f.fecha))
-JOIN LA_EMPRESA.servicio s ON (s.codigo = f.id_servicio)
-JOIN LA_EMPRESA.compra co ON (co.numero = s.id_compra)
-JOIN LA_EMPRESA.vuelo v ON v.codigo = p.id_vuelo
-JOIN LA_EMPRESA.ruta_aerea ra ON v.id_ruta_aerea = ra.id
+FROM LOW_STRESS_DESIGN.pasaje p
+JOIN LOW_STRESS_DESIGN.factura f ON f.id_servicio = p.codigo
+JOIN LOW_STRESS_DESIGN.bi_anio_y_mes aym ON (aym.anio = YEAR(f.fecha) AND aym.mes = MONTH(f.fecha))
+JOIN LOW_STRESS_DESIGN.servicio s ON (s.codigo = f.id_servicio)
+JOIN LOW_STRESS_DESIGN.compra co ON (co.numero = s.id_compra)
+JOIN LOW_STRESS_DESIGN.vuelo v ON v.codigo = p.id_vuelo
+JOIN LOW_STRESS_DESIGN.ruta_aerea ra ON v.id_ruta_aerea = ra.id
 GROUP BY aym.id, f.id_cliente, co.id_empresa, ra.id_ciudad_orig, ra.id_ciudad_dest, v.id_avion, ra.codigo;
 
 PRINT 'Llenado de Fact Table Pasaje completo';
--- ScriptsDB/script_migracion.sql
+++ ScriptsDB/script_migracion.sql
@@ -1,22 +1,22 @@
 USE [GD1C2020]
 GO
 --CREACION DE SCHEMA
-CREATE SCHEMA [LA_EMPRESA]
+CREATE SCHEMA [LOW_STRESS_DESIGN]
 GO
 
-CREATE PROCEDURE LA_EMPRESA.SP_MIGRATION_SCHEMA
+CREATE PROCEDURE LOW_STRESS_DESIGN.SP_MIGRATION_SCHEMA
 AS
 	
 	--CREACION DE TABLAS
 	PRINT 'Comienza la creacion de tablas.'
 
-	CREATE TABLE LA_EMPRESA.empresa (
+	CREATE TABLE LOW_STRESS_DESIGN.empresa (
 		id INTEGER IDENTITY(0,1) PRIMARY KEY,
 		razon_social VARCHAR(100) NOT NULL
 	);
 	PRINT 'Creada tabla empresa.'
 
-	CREATE TABLE LA_EMPRESA.sucursal(
+	CREATE TABLE LOW_STRESS_DESIGN.sucursal(
 		id INTEGER IDENTITY(0,1) PRIMARY KEY,
 		dir VARCHAR(50) NOT NULL,
 		mail VARCHAR(50) NOT NULL,
@@ -24,7 +24,7 @@ AS
 	);
 	PRINT 'Creada tabla sucursal.'
 
-	CREATE TABLE LA_EMPRESA.ciudad (
+	CREATE TABLE LOW_STRESS_DESIGN.ciudad (
 		codigo INT IDENTITY(0,1) PRIMARY KEY,
 		nombre VARCHAR(50) NOT NULL
 	);
@@ -38,7 +38,7 @@ AS
 
 
 
-	CREATE TABLE LA_EMPRESA.cliente (
+	CREATE TABLE LOW_STRESS_DESIGN.cliente (
 		id INTEGER IDENTITY(0,1) PRIMARY KEY,
 		dni INTEGER NOT NULL,
 		apellido VARCHAR(50) NOT NULL,
@@ -49,38 +49,38 @@ AS
 	);
 	PRINT 'Creada tabla cliente.'
 
-	CREATE TABLE LA_EMPRESA.tipo_habitacion (
+	CREATE TABLE LOW_STRESS_DESIGN.tipo_habitacion (
 		codigo INT PRIMARY KEY,
 		descripcion VARCHAR(50) NOT NULL
 	);
 	PRINT 'Creada tabla tipo_habitacion.'
 
-	CREATE TABLE LA_EMPRESA.compra(
+	CREATE TABLE LOW_STRESS_DESIGN.compra(
 		numero INT PRIMARY KEY,
 		id_empresa int NOT NULL,
 		fecha DATETIME NOT NULL,
-		FOREIGN KEY (id_empresa) REFERENCES LA_EMPRESA.empresa (id)
+		FOREIGN KEY (id_empresa) REFERENCES LOW_STRESS_DESIGN.empresa (id)
 	);
 	PRINT 'Creada tabla compra.'
 
-	CREATE TABLE LA_EMPRESA.servicio(
+	CREATE TABLE LOW_STRESS_DESIGN.servicio(
 		codigo INT PRIMARY KEY,
 		id_compra int NOT NULL,
-		FOREIGN KEY (id_compra) REFERENCES LA_EMPRESA.compra (numero)
+		FOREIGN KEY (id_compra) REFERENCES LOW_STRESS_DESIGN.compra (numero)
 	);
 	PRINT 'Creada tabla servicio.'
 
-	CREATE TABLE LA_EMPRESA.hotel(
+	CREATE TABLE LOW_STRESS_DESIGN.hotel(
 		id_hotel INT IDENTITY(0,1) PRIMARY KEY,
 		id_empresa int,
 		calle VARCHAR(50),
 		nro_calle int,
 		cantidad_estrellas int,
-		FOREIGN KEY (id_empresa) REFERENCES LA_EMPRESA.empresa (id)
+		FOREIGN KEY (id_empresa) REFERENCES LOW_STRESS_DESIGN.empresa (id)
 	);
 	PRINT 'Creada tabla hotel.'
 
-	CREATE TABLE LA_EMPRESA.habitacion(
+	CREATE TABLE LOW_STRESS_DESIGN.habitacion(
 		id_habitacion INTEGER IDENTITY(0,1) PRIMARY KEY,
 		id_hotel INTEGER NOT NULL,
 		id_tipo_habitacion INTEGER NOT NULL,
@@ -89,43 +89,43 @@ AS
 		frente VARCHAR(5) NOT NULL,
 		costo INTEGER NOT NULL,
 		precio INTEGER NOT NULL,
-		FOREIGN KEY (id_hotel) REFERENCES LA_EMPRESA.hotel (id_hotel),
-		FOREIGN KEY (id_tipo_habitacion) REFERENCES LA_EMPRESA.tipo_habitacion (codigo)
+		FOREIGN KEY (id_hotel) REFERENCES LOW_STRESS_DESIGN.hotel (id_hotel),
+		FOREIGN KEY (id_tipo_habitacion) REFERENCES LOW_STRESS_DESIGN.tipo_habitacion (codigo)
 	);
 	PRINT 'Creada tabla habitacion.'
 
-	CREATE TABLE LA_EMPRESA.estadia(
+	CREATE TABLE LOW_STRESS_DESIGN.estadia(
 		codigo INTEGER PRIMARY KEY,
 		id_habitacion INTEGER NOT NULL,
 		fecha_ini DATETIME NOT NULL,
 		cantidad_noches INTEGER NOT NULL,
-		FOREIGN KEY (id_habitacion) REFERENCES LA_EMPRESA.habitacion (id_habitacion)
+		FOREIGN KEY (id_habitacion) REFERENCES LOW_STRESS_DESIGN.habitacion (id_habitacion)
 	);
 
 	PRINT 'Creada tabla estadia.'
 
-	CREATE TABLE LA_EMPRESA.factura(
+	CREATE TABLE LOW_STRESS_DESIGN.factura(
 		nro INTEGER PRIMARY KEY,
 		id_servicio INTEGER NOT NULL,
 		id_cliente INTEGER NOT NULL,
 		id_sucursal INTEGER NOT NULL,
 		fecha DATETIME,
-		FOREIGN KEY (id_servicio) REFERENCES LA_EMPRESA.servicio (codigo),
-		FOREIGN KEY (id_cliente) REFERENCES LA_EMPRESA.cliente (id),
-		FOREIGN KEY (id_sucursal) REFERENCES LA_EMPRESA.sucursal (id)
+		FOREIGN KEY (id_servicio) REFERENCES LOW_STRESS_DESIGN.servicio (codigo),
+		FOREIGN KEY (id_cliente) REFERENCES LOW_STRESS_DESIGN.cliente (id),
+		FOREIGN KEY (id_sucursal) REFERENCES LOW_STRESS_DESIGN.sucursal (id)
 	);
 	PRINT 'Creada tabla factura.'
 
-	CREATE TABLE LA_EMPRESA.avion (
+	CREATE TABLE LOW_STRESS_DESIGN.avion (
 		identificador VARCHAR(50) NOT NULL PRIMARY KEY,
-		id_empresa INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.empresa(id),
+		id_empresa INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.empresa(id),
 		modelo VARCHAR(50)
 	);
 	PRINT 'Creada tabla avion.'
 
-	CREATE TABLE LA_EMPRESA.butaca (
+	CREATE TABLE LOW_STRESS_DESIGN.butaca (
 		id INT IDENTITY(0,1) PRIMARY KEY,
-		id_avion VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.avion(identificador),
+		id_avion VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.avion(identificador),
 		numero INT NOT NULL,
 		tipo VARCHAR(50)
 	);
@@ -134,40 +134,40 @@ AS
 
 	-- El campo id sirve para identificar a la ruta aerea direccionada
 	-- El campo codigo sirve para identificar a la ruta aerea sin importar direccionamiento
-	CREATE TABLE LA_EMPRESA.ruta_aerea (
+	CREATE TABLE LOW_STRESS_DESIGN.ruta_aerea (
 		id INT IDENTITY(0,1) PRIMARY KEY,
 		codigo INT NOT NULL,
-		id_ciudad_orig INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.ciudad(codigo),
-		id_ciudad_dest INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.ciudad(codigo)
+		id_ciudad_orig INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.ciudad(codigo),
+		id_ciudad_dest INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.ciudad(codigo)
 	);
 	PRINT 'Creada tabla ruta_aerea.'
 
-	CREATE TABLE LA_EMPRESA.vuelo (
+	CREATE TABLE LOW_STRESS_DESIGN.vuelo (
 		codigo INT NOT NULL PRIMARY KEY,
-		id_avion VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.avion(identificador),
-		id_ruta_aerea INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.ruta_aerea(id),
+		id_avion VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.avion(identificador),
+		id_ruta_aerea INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.ruta_aerea(id),
 		fecha_salida DATETIME,
 		fecha_llegada DATETIME
 	);
 	PRINT 'Creada tabla vuelo.'
 
-	CREATE TABLE LA_EMPRESA.pasaje (
-		codigo INT NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES LA_EMPRESA.servicio(codigo),
-		id_vuelo INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.vuelo(codigo),
+	CREATE TABLE LOW_STRESS_DESIGN.pasaje (
+		codigo INT NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.servicio(codigo),
+		id_vuelo INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.vuelo(codigo),
 		costo DECIMAL(18,2) NOT NULL,
 		precio DECIMAL(18,2) NOT NULL,
 		fecha_compra DATETIME
 	);
 	PRINT 'Creada tabla pasaje.'
 
-	CREATE TABLE LA_EMPRESA.pasaje_butaca (
-		id_pasaje INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.pasaje(codigo),
-		id_butaca INT NOT NULL FOREIGN KEY REFERENCES LA_EMPRESA.butaca(id),
+	CREATE TABLE LOW_STRESS_DESIGN.pasaje_butaca (
+		id_pasaje INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.pasaje(codigo),
+		id_butaca INT NOT NULL FOREIGN KEY REFERENCES LOW_STRESS_DESIGN.butaca(id),
 		PRIMARY KEY (id_pasaje, id_butaca)
 	);
 	PRINT 'Creada tabla pasaje_butaca'
 GO
-EXECUTE LA_EMPRESA.SP_MIGRATION_SCHEMA
+EXECUTE LOW_STRESS_DESIGN.SP_MIGRATION_SCHEMA
 PRINT 'Creadas tablas del esquema'
 GO
 
@@ -175,7 +175,7 @@ GO
 
 --------------------------- MIGRACION DE DATOS ---------------------------
 
-INSERT INTO LA_EMPRESA.empresa (razon_social)
+INSERT INTO LOW_STRESS_DESIGN.empresa (razon_social)
 SELECT DISTINCT  EMPRESA_RAZON_SOCIAL
 FROM gd_esquema.Maestra M 
 WHERE EMPRESA_RAZON_SOCIAL IS NOT NULL ORDER BY 1
@@ -184,7 +184,7 @@ GO
 PRINT 'Empresas migradas'
 GO
 
-INSERT INTO LA_EMPRESA.sucursal (dir, mail, telefono)
+INSERT INTO LOW_STRESS_DESIGN.sucursal (dir, mail, telefono)
 select distinct SUCURSAL_DIR, SUCURSAL_MAIL, SUCURSAL_TELEFONO
 FROM gd_esquema.Maestra
 WHERE (SUCURSAL_DIR is not null or SUCURSAL_MAIL is not null or SUCURSAL_TELEFONO is not null);
@@ -192,7 +192,7 @@ GO
 PRINT 'Sucursales migradas'
 GO
 
-INSERT INTO LA_EMPRESA.ciudad (nombre)
+INSERT INTO LOW_STRESS_DESIGN.ciudad (nombre)
 SELECT DISTINCT m.RUTA_AEREA_CIU_DEST as CIUDADES
 FROM gd_esquema.Maestra m 
 WHERE m.RUTA_AEREA_CIU_DEST is not null union
@@ -204,14 +204,14 @@ GO
 PRINT 'Ciudades migradas'
 GO
 
-INSERT INTO LA_EMPRESA.cliente (nombre, apellido, dni, email, telefono, fecha_nac)
+INSERT INTO LOW_STRESS_DESIGN.cliente (nombre, apellido, dni, email, telefono, fecha_nac)
 SELECT distinct m.CLIENTE_NOMBRE, m.CLIENTE_APELLIDO, CAST(m.CLIENTE_DNI as INTEGER) as DNI, m.CLIENTE_MAIL, CAST(m.CLIENTE_TELEFONO as INTEGER) as TELEFONO, CAST(m.CLIENTE_FECHA_NAC as DATETIME)
 FROM gd_esquema.Maestra m
 WHERE m.CLIENTE_DNI is not null order by m.CLIENTE_NOMBRE, m.CLIENTE_APELLIDO;
 
 PRINT 'Clientes migrados'
 
-INSERT INTO LA_EMPRESA.tipo_habitacion (codigo, descripcion)
+INSERT INTO LOW_STRESS_DESIGN.tipo_habitacion (codigo, descripcion)
 SELECT DISTINCT TIPO_HABITACION_CODIGO, TIPO_HABITACION_DESC 
 FROM gd_esquema.Maestra 
 WHERE TIPO_HABITACION_CODIGO is not null or TIPO_HABITACION_DESC is not null;
@@ -219,63 +219,63 @@ WHERE TIPO_HABITACION_CODIGO is not null or TIPO_HABITACION_DESC is not null;
 PRINT 'Tipo de habitaciones migradas'
 
 
-INSERT INTO LA_EMPRESA.hotel (id_empresa, calle, cantidad_estrellas, nro_calle)
+INSERT INTO LOW_STRESS_DESIGN.hotel (id_empresa, calle, cantidad_estrellas, nro_calle)
 SELECT DISTINCT E.id, M.HOTEL_CALLE, M.HOTEL_CANTIDAD_ESTRELLAS, M.HOTEL_NRO_CALLE
-FROM LA_EMPRESA.empresa E
+FROM LOW_STRESS_DESIGN.empresa E
 JOIN gd_esquema.Maestra M ON M.EMPRESA_RAZON_SOCIAL = E.razon_social
 WHERE M.HOTEL_CALLE IS NOT NULL ORDER BY E.id
 
 PRINT 'Hoteles migrados'
 
-INSERT INTO LA_EMPRESA.habitacion (id_hotel, id_tipo_habitacion, numero, piso, precio, frente, costo)
+INSERT INTO LOW_STRESS_DESIGN.habitacion (id_hotel, id_tipo_habitacion, numero, piso, precio, frente, costo)
 SELECT DISTINCT H.id_hotel ,TA.codigo, HABITACION_NUMERO, HABITACION_PISO,HABITACION_PRECIO, CAST(HABITACION_FRENTE AS varchar) ,HABITACION_COSTO
-FROM LA_EMPRESA.hotel H
+FROM LOW_STRESS_DESIGN.hotel H
 JOIN gd_esquema.Maestra M ON H.calle = M.HOTEL_CALLE
-JOIN LA_EMPRESA.tipo_habitacion TA ON M.TIPO_HABITACION_DESC = TA.descripcion
+JOIN LOW_STRESS_DESIGN.tipo_habitacion TA ON M.TIPO_HABITACION_DESC = TA.descripcion
 WHERE HABITACION_COSTO IS NOT NULL;
 
 PRINT 'Tipo de habitaciones migradas'
 
-INSERT INTO LA_EMPRESA.avion (identificador, id_empresa, modelo)
+INSERT INTO LOW_STRESS_DESIGN.avion (identificador, id_empresa, modelo)
 SELECT DISTINCT m.AVION_IDENTIFICADOR, e.id, m.AVION_MODELO
 FROM gd_esquema.Maestra as m
-         JOIN LA_EMPRESA.empresa as e ON m.EMPRESA_RAZON_SOCIAL = e.razon_social
+         JOIN LOW_STRESS_DESIGN.empresa as e ON m.EMPRESA_RAZON_SOCIAL = e.razon_social
 WHERE m.AVION_IDENTIFICADOR IS NOT NULL
 
 PRINT 'Aviones migrados.'
 
-INSERT INTO LA_EMPRESA.ruta_aerea (codigo, id_ciudad_orig, id_ciudad_dest)
+INSERT INTO LOW_STRESS_DESIGN.ruta_aerea (codigo, id_ciudad_orig, id_ciudad_dest)
 SELECT DISTINCT m.RUTA_AEREA_CODIGO, c1.codigo, c2.codigo
 FROM gd_esquema.Maestra as m
-         JOIN LA_EMPRESA.ciudad as c1 ON m.RUTA_AEREA_CIU_ORIG = c1.nombre
-         JOIN LA_EMPRESA.ciudad as c2 ON m.RUTA_AEREA_CIU_DEST = c2.nombre
+         JOIN LOW_STRESS_DESIGN.ciudad as c1 ON m.RUTA_AEREA_CIU_ORIG = c1.nombre
+         JOIN LOW_STRESS_DESIGN.ciudad as c2 ON m.RUTA_AEREA_CIU_DEST = c2.nombre
 
 PRINT 'Rutas aereas migradas.'
 
-INSERT INTO LA_EMPRESA.vuelo (codigo, id_avion, id_ruta_aerea, fecha_salida, fecha_llegada)
+INSERT INTO LOW_STRESS_DESIGN.vuelo (codigo, id_avion, id_ruta_aerea, fecha_salida, fecha_llegada)
 SELECT DISTINCT m.VUELO_CODIGO, m.AVION_IDENTIFICADOR, ra.id, m.VUELO_FECHA_SALUDA, m.VUELO_FECHA_LLEGADA
-FROM LA_EMPRESA.ruta_aerea as ra
-         JOIN LA_EMPRESA.ciudad as c1 ON ra.id_ciudad_orig = c1.codigo
-         JOIN LA_EMPRESA.ciudad as c2 ON ra.id_ciudad_dest = c2.codigo
+FROM LOW_STRESS_DESIGN.ruta_aerea as ra
+         JOIN LOW_STRESS_DESIGN.ciudad as c1 ON ra.id_ciudad_orig = c1.codigo
+         JOIN LOW_STRESS_DESIGN.ciudad as c2 ON ra.id_ciudad_dest = c2.codigo
          JOIN gd_esquema.Maestra as m ON (m.RUTA_AEREA_CODIGO = ra.codigo AND m.RUTA_AEREA_CIU_ORIG = c1.nombre AND m.RUTA_AEREA_CIU_DEST = c2.nombre)
 
 PRINT 'Vuelos migrados.'
 
-INSERT INTO LA_EMPRESA.butaca (id_avion, numero, tipo)
+INSERT INTO LOW_STRESS_DESIGN.butaca (id_avion, numero, tipo)
 SELECT DISTINCT m.AVION_IDENTIFICADOR, m.BUTACA_NUMERO, m.BUTACA_TIPO
 FROM gd_esquema.Maestra as m
 where m.AVION_IDENTIFICADOR is not null AND m.BUTACA_NUMERO is not null
 
 PRINT 'Butacas migradas.'
 
-INSERT INTO LA_EMPRESA.compra (numero, id_empresa, fecha)
+INSERT INTO LOW_STRESS_DESIGN.compra (numero, id_empresa, fecha)
 SELECT DISTINCT m.COMPRA_NUMERO, e.id, m.COMPRA_FECHA
 FROM gd_esquema.Maestra as m
-JOIN LA_EMPRESA.empresa as e ON m.EMPRESA_RAZON_SOCIAL = e.razon_social
+JOIN LOW_STRESS_DESIGN.empresa as e ON m.EMPRESA_RAZON_SOCIAL = e.razon_social
 
 PRINT 'Compras migradas.'
 
-INSERT INTO LA_EMPRESA.servicio (codigo, id_compra)
+INSERT INTO LOW_STRESS_DESIGN.servicio (codigo, id_compra)
 SELECT DISTINCT m.ESTADIA_CODIGO, m.COMPRA_NUMERO 
 FROM gd_esquema.Maestra as m
 WHERE m.ESTADIA_CODIGO IS NOT NULL
@@ -286,36 +286,36 @@ WHERE m.PASAJE_CODIGO IS NOT NULL
 
 PRINT 'Servicios migrados.'
 
-INSERT INTO LA_EMPRESA.pasaje (codigo, id_vuelo, costo, precio, fecha_compra)
+INSERT INTO LOW_STRESS_DESIGN.pasaje (codigo, id_vuelo, costo, precio, fecha_compra)
 SELECT DISTINCT m.PASAJE_CODIGO, m.VUELO_CODIGO, m.PASAJE_COSTO, m.PASAJE_PRECIO, m.PASAJE_FECHA_COMPRA
 FROM gd_esquema.Maestra as m
 WHERE m.PASAJE_CODIGO IS NOT NULL
 
 PRINT 'Pasajes migrados.'
 
-INSERT INTO LA_EMPRESA.pasaje_butaca (id_pasaje, id_butaca)
+INSERT INTO LOW_STRESS_DESIGN.pasaje_butaca (id_pasaje, id_butaca)
 SELECT DISTINCT m.PASAJE_CODIGO, b.id
 FROM gd_esquema.Maestra as m
-JOIN LA_EMPRESA.butaca as b ON (m.AVION_IDENTIFICADOR = b.id_avion AND m.BUTACA_NUMERO = b.numero)
+JOIN LOW_STRESS_DESIGN.butaca as b ON (m.AVION_IDENTIFICADOR = b.id_avion AND m.BUTACA_NUMERO = b.numero)
 
 PRINT 'Pasajes x butacas migrados.'
 
-INSERT INTO LA_EMPRESA.estadia (codigo, id_habitacion, fecha_ini, cantidad_noches)
+INSERT INTO LOW_STRESS_DESIGN.estadia (codigo, id_habitacion, fecha_ini, cantidad_noches)
 SELECT DISTINCT m.ESTADIA_CODIGO, ha.id_habitacion , m.ESTADIA_FECHA_INI, m.ESTADIA_CANTIDAD_NOCHES
-FROM LA_EMPRESA.empresa as e
-JOIN LA_EMPRESA.hotel as ho ON e.id = ho.id_empresa
-JOIN LA_EMPRESA.habitacion as ha ON ho.id_hotel = ha.id_hotel
+FROM LOW_STRESS_DESIGN.empresa as e
+JOIN LOW_STRESS_DESIGN.hotel as ho ON e.id = ho.id_empresa
+JOIN LOW_STRESS_DESIGN.habitacion as ha ON ho.id_hotel = ha.id_hotel
 JOIN gd_esquema.Maestra as m ON (m.EMPRESA_RAZON_SOCIAL = e.razon_social AND m.HOTEL_CALLE = ho.calle AND m.HABITACION_NUMERO = ha.numero AND m.HABITACION_PISO = ha.piso)
 
 PRINT 'Estadias migradas.'
 
-INSERT INTO LA_EMPRESA.factura (id_servicio, id_cliente, id_sucursal, nro, fecha)
-SELECT DISTINCT M.PASAJE_CODIGO, CLI.id, SU.id, M.FACTURA_NRO, M.FACTURA_FECHA FROM LA_EMPRESA.cliente CLI 
+INSERT INTO LOW_STRESS_DESIGN.factura (id_servicio, id_cliente, id_sucursal, nro, fecha)
+SELECT DISTINCT M.PASAJE_CODIGO, CLI.id, SU.id, M.FACTURA_NRO, M.FACTURA_FECHA FROM LOW_STRESS_DESIGN.cliente CLI
 JOIN gd_esquema.Maestra M ON M.CLIENTE_DNI = CLI.dni AND M.CLIENTE_NOMBRE = CLI.nombre AND M.CLIENTE_APELLIDO = CLI.apellido
-JOIN LA_EMPRESA.sucursal SU ON SU.dir = M.SUCURSAL_DIR WHERE M.PASAJE_CODIGO IS NOT NULL
+JOIN LOW_STRESS_DESIGN.sucursal SU ON SU.dir = M.SUCURSAL_DIR WHERE M.PASAJE_CODIGO IS NOT NULL
 UNION
-SELECT DISTINCT M.ESTADIA_CODIGO, CLI.id, SU.id, M.FACTURA_NRO, M.FACTURA_FECHA FROM LA_EMPRESA.cliente CLI
+SELECT DISTINCT M.ESTADIA_CODIGO, CLI.id, SU.id, M.FACTURA_NRO, M.FACTURA_FECHA FROM LOW_STRESS_DESIGN.cliente CLI
 JOIN gd_esquema.Maestra M ON M.CLIENTE_DNI = CLI.dni AND M.CLIENTE_NOMBRE = CLI.nombre AND M.CLIENTE_APELLIDO = CLI.apellido
-JOIN LA_EMPRESA.sucursal SU ON SU.dir = M.SUCURSAL_DIR WHERE M.ESTADIA_CODIGO IS NOT NULL
+JOIN LOW_STRESS_DESIGN.sucursal SU ON SU.dir = M.SUCURSAL_DIR WHERE M.ESTADIA_CODIGO IS NOT NULL
 
 PRINT 'Facturas migradas.'
